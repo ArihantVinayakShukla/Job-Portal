fae2d705e2428f3756847c156516870feb53dcd8
Arihant <shuklaarihant0@gmail.com>
2023-07-25 19:07:35 +0530

    Sixth commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import RegistrationPage from "./Components/RegistrationPage";
 
 function App() {
   return (
-
-<<<<<<< HEAD
+<>
     <RegistrationPage/>
     
-=======
-    <>
       <h1>juyhucguwujkdl</h1>
     </>
->>>>>>> 49753acac0c1b8e1d8dd332938a62d63bd31e43d
   );
 }
 
