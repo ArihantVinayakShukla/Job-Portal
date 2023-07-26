8f4abc3734f5f07b10ab192ea330ed760d97e94e
Garvit1008 <garvitdang10august@gmail.com>
2023-07-26 13:41:57 +0530

    jobpreference commit

---
+++ src/Components/RegistrationPage/RegistrationForms/JobPreferences/JobPreference.jsx
@@ -0,0 +1,87 @@
+import React ,{useState} from "react";
+import './JobPreference.css'
+import Select from "react-select";
+
+function JobPreference(){
+    const [selectedOptions, setSelectedOptions] = useState();
+
+    // Array of all options
+    const optionList = [
+      { value: "banglore", label: "Banglore" },
+      { value: "delhi", label: "Delhi" },
+      { value: "mumbai", label: "Mumbai" },
+      { value: "Noida", label: "Noida" },
+      { value: "pune", label: "Pune" },
+      { value: "agra", label: "Agra" },
+      { value: "ghaziabad", label: "Ghaziabad" },
+      { value: "jaipur", label: "Jaipur" },
+      { value: "hyderabad", label: "Hyderabad" },
+      { value: "gurugram", label: "GuruGram" },
+    ];
+    function handleSelect(data) {
+        setSelectedOptions(data);
+      }
+return(
+     <article className="jobContainer">
+        <div>
+    <h1 className='jobHeading'>
+      Job Preferences
+    </h1>
+    <p className="jobHeadingPara">Helps to find better matching jobs</p>
+    </div>
+    <form className="basicForm">
+        <div className="fieldGroup">
+        <label  className="heading">
+            Preferred Location
+
+          </label>
+      <div className="dropdown-container">
+        <Select
+          options={optionList}
+          placeholder="Select Locations"
+          value={selectedOptions}
+          onChange={handleSelect}
+          isSearchable={true}
+          isMulti
+        />
+      </div>
+    </div>
+        <h2 className="heading">What are you currently looking for?</h2>
+    <div className='checkboxDiv'>
+        <input type="radio" name="profession" required/>
+        <div>
+        <h2 className="value">Freshers Job</h2>
+        </div>
+        
+      </div>
+      <div className='checkboxDiv'>
+        <input type="radio" name="profession" required/>
+        <div>
+        <h2 className="value">Internship</h2>
+        </div>
+      </div>
+      <h2 className="heading">Job type</h2>
+      <div className="checkboxDiv">
+        <input type="radio" name="typejob" required></input>
+        <div>
+            <h2 className="value">Full time</h2>
+        </div>
+      </div>
+      <div className="checkboxDiv">
+        <input type="radio" name="typejob" required></input>
+        <div>
+            <h2 className="value">Part time</h2>
+        </div>
+      </div>
+      <button type="submit" className="continue-btn">
+        <div class="continue-btn-wrapper">
+          <div id="continue-btn" className="primaryBtn commonBtn continueBtn">
+            <span class="cont-text">Job Hunt!</span>
+          </div>
+        </div>
+            </button>
+    </form>
+    </article>
+)
+}
+export default JobPreference;
--- src/Components/RegistrationPage/RegistrationForms/RegistrationForms.jsx
+++ src/Components/RegistrationPage/RegistrationForms/RegistrationForms.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './RegistrationForms.css'
 // import BasicDetails from './BasicDetails/BasicDetails.jsx'
-// import ProfessionalDetails from './ProfessionalDetails/ProfessionalDetails.jsx'
+import ProfessionalDetails from './ProfessionalDetails/ProfessionalDetails.jsx'
 import EducationDetails from './EducationDetails/EducationDetails.jsx'
-
+import JobPreference from './JobPreferences/JobPreference'
 const RegistrationForms = (props) => {
   return (
     <article className='form-article'>
     {/* <BasicDetails/> */}
     {/* <ProfessionalDetails/> */}
-    <EducationDetails/>
+    {/* <EducationDetails/> */}
+    <JobPreference></JobPreference>
     </article>
   )
 }
