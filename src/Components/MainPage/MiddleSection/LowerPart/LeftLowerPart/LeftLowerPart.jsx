979ee7e02ef3acfe0ed138c04689446c747ff34c
Harsh Verma <harshverma8433@gmail.com>
2023-07-30 00:54:50 +0530

    left-right lower part responsive

--- src/Components/MainPage/MiddleSection/LowerPart/LeftLowerPart/LeftLowerPart.jsx
+++ src/Components/MainPage/MiddleSection/LowerPart/LeftLowerPart/LeftLowerPart.jsx
@@ -1,48 +1,49 @@
-import React from 'react';
-import "./LeftLowerPart.css"
-import {BsClockHistory} from "react-icons/bs"
-import {PiBuildingsLight} from "react-icons/pi"
-import {PiSuitcaseSimpleDuotone} from "react-icons/pi"
-const LeftLowerPart = ({jobTitle ,  companyName , jobType , jobLogo ,jobExperience}) => {
+import React from "react";
+import "./LeftLowerPart.css";
+import { BsClockHistory } from "react-icons/bs";
+import { PiBuildingsLight } from "react-icons/pi";
+import { PiSuitcaseSimpleDuotone } from "react-icons/pi";
+const LeftLowerPart = ({
+  jobTitle,
+  companyName,
+  jobType,
+  jobLogo,
+  jobExperience,
+}) => {
   return (
     <>
-      <article className='leftlower'>
-        
-          <div className='joblogo'>
-            <img src={jobLogo} alt="" />
-          </div>
-          <div className='descWithBtn'>
-          <div className='descWithHeading'>
-          <h1>{jobTitle}</h1>
-         
-          <div className='jobdescription'>
+      <article className="leftlower">
+        <div className="joblogo">
+          <img src={jobLogo} alt="" />
+        </div>
+        <div className="descWithBtn">
+          <div className="descWithHeading">
+            <h2>{jobTitle}</h2>
 
-              <div className='cname'>
-                <PiBuildingsLight/>
-                <h2>{companyName}</h2>
+            <div className="jobdescription">
+              <div className="cname">
+                <PiBuildingsLight />
+                <h3>{companyName}</h3>
               </div>
 
-              <div className='expJob'>
-                <PiSuitcaseSimpleDuotone/>
+              <div className="expJob">
+                <PiSuitcaseSimpleDuotone />
                 <h3>{jobExperience}</h3>
               </div>
 
-              <div className='typejob'>
-                <BsClockHistory/>
+              <div className="typejob">
+                <BsClockHistory />
                 <h3>{jobType}</h3>
               </div>
-
-          
+            </div>
+          </div>
+          <div className="jobBtnDiv">
+            <button className="jobBtn">Apply</button>
+          </div>
         </div>
-        </div>
-        <div className='jobBtnDiv'>
-          <button className='jobBtn'>Apply</button>
-</div>
-</div>
-        
       </article>
     </>
-  )
-}
+  );
+};
 
 export default LeftLowerPart;
