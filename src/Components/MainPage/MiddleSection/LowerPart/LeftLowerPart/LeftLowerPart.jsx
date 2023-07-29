02a81d9626c3e28d6688546d140fb7479493cded
Harsh Verma <harshverma8433@gmail.com>
2023-07-29 18:30:36 +0530

    Middle Section

--- src/Components/MainPage/MiddleSection/LowerPart/LeftLowerPart/LeftLowerPart.jsx
+++ src/Components/MainPage/MiddleSection/LowerPart/LeftLowerPart/LeftLowerPart.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
-const LeftLowerPart = () => {
+import "./LeftLowerPart.css"
+import {BsClockHistory} from "react-icons/bs"
+import {PiBuildingsLight} from "react-icons/pi"
+import {PiSuitcaseSimpleDuotone} from "react-icons/pi"
+const LeftLowerPart = ({jobTitle ,  companyName , jobType , jobLogo ,jobExperience}) => {
   return (
-   <h1>Lambi meri bandi Hot Hot Long Long Legs</h1>
+    <>
+      <article className='leftlower'>
+        
+          <div className='joblogo'>
+            <img src={jobLogo} alt="" />
+          </div>
+          <h1>{jobTitle}</h1>
+         
+          <div className='jobdescription'>
+
+              <div className='cname'>
+                <PiBuildingsLight/>
+                <h2>{companyName}</h2>
+              </div>
+
+              <div className='expJob'>
+                <PiSuitcaseSimpleDuotone/>
+                <h3>{jobExperience}</h3>
+              </div>
+
+              <div className='typejob'>
+                <BsClockHistory/>
+                <h3>{jobType}</h3>
+
+              </div>
+
+          
+        </div>
+          <button>Apply</button>
+
+        
+      </article>
+    </>
   )
 }
 
