68699f7a432eb019eb58a7fbb73e55e28fb1c821
Harsh Verma <harshverma8433@gmail.com>
2023-07-30 22:35:18 +0530

    Footer Commit

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import './MainPage.css'
 import UpperSection from './UpperSection/UpperSection';
-import MiddleSection from './MiddleSection/MiddleSection.jsx'
+import MiddleSection from './MiddleSection/MiddleSection.jsx';
+import Loader from '../RegistrationPage/Loader';
+import {useState , useEffect} from "react";
+import Footer from './Footer/Footer';
 
 const MainPage = () => {
+
+  
+  const [loader , setloader] = useState(true);
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setloader(false);
+    },2000)
+  },[])
   return (
     <>
-    <div className='outermostDiv'>
-      <article className='mainArticle'>
-          <UpperSection/>
-          <MiddleSection/>
-      </article>
-      
-    </div>
+
+    {
+      loader ? <Loader/>:
+    
+      <div className='outermostDiv'>
+        <article className='mainArticle'>
+            <UpperSection/>
+            <MiddleSection/>
+        </article>
+
+      <Footer/>
+        
+      </div>
+    }
     </>
   )
 }
